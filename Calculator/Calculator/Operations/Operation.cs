24a45ad5a17284a42696d1056f2582ff392a0b28
Gal Tamir <galtamir@microsoft.com>
2020-11-21 10:44:34 +0200

    a

---
+++ Calculator/Calculator/Operations/Operation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Operations
+{
+    public class Operation
+    {
+        public static Operation Addition = new Operation((a, b) => a + b);
+
+        public Uri Icon { get; }
+
+        public OperationOrder Order { get; }
+
+        public Func<double, double, double> Func { get; }
+        
+        private Operation(Func<double, double, double> func)
+        {
+            Func = func;
+        }
+    }
+}
